86a1b71c25e18c08f03d03b2ee9314a075e7a1b0
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-03-24 13:36:57 +0100

    updated to cache backend

--- storagecelery.py
+++ storagecelery.py
@@ -4,13 +4,15 @@ from socket import gethostname
 from os import getenv
 HOSTNAME = gethostname()
 AMQP_URI = getenv('AMQP_URI')
+CACHE_URI = getenv('CACHE_URI')
 
-
-celery = Celery('storagedriver', backend='amqp',
+celery = Celery('storagedriver',
                 broker=AMQP_URI,
                 include=['storagedriver'])
 
 celery.conf.update(
+    CELERY_RESULT_BACKEND='cache',
+    CELERY_CACHE_BACKEND=CACHE_URI,
     CELERY_TASK_RESULT_EXPIRES=300,
     CELERY_QUEUES=(
         Queue(HOSTNAME + '.storage', Exchange(
