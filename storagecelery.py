68cc93e8ab1cc0ac765dec3a34fb699bd4919fb4
Bach Dániel <bd@ik.bme.hu>
2015-02-27 17:03:46 +0100

    change celery result backend

--- storagecelery.py
+++ storagecelery.py
@@ -16,15 +16,13 @@ if HOSTNAME is None:
                     "Hostname format must be hostname.module.priority.")
 
 AMQP_URI = getenv('AMQP_URI')
-CACHE_URI = getenv('CACHE_URI')
 
 celery = Celery('storagedriver',
                 broker=AMQP_URI,
                 include=['storagedriver'])
 
 celery.conf.update(
-    CELERY_RESULT_BACKEND='cache',
-    CELERY_CACHE_BACKEND=CACHE_URI,
+    CELERY_RESULT_BACKEND='amqp',
     CELERY_TASK_RESULT_EXPIRES=300,
     CELERY_QUEUES=(
         Queue(HOSTNAME, Exchange(
